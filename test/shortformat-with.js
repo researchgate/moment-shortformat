711453e89059cbcc50644d1a15e90c67c4d37375
Daniel Tschinder <code@tschinder.de>
2015-10-15 15:59:31 +0200

    Fix tests

--- test/shortformat-with.js
+++ test/shortformat-with.js
@@ -3,64 +3,64 @@ var moment = require('../moment-shortformat');
 
 test('test seconds ago with suffix', function (t) {
     t.plan(1);
-    t.equal(moment().subtract(25.5, 'seconds').short(), '25s ago');
+    t.equal(moment().subtract(25, 'seconds').short(), '25s ago');
 });
 
 test('test seconds in future with prefix', function (t) {
     t.plan(1);
-    t.equal(moment().add(25.5, 'seconds').short(), 'in 25s');
+    t.equal(moment().add(25, 'seconds').short(), 'in 25s');
 });
 
 test('test minutes ago with suffix', function (t) {
     t.plan(1);
-    t.equal(moment().subtract(5.5, 'minutes').short(), '5m ago');
+    t.equal(moment().subtract(5, 'minutes').short(), '5m ago');
 });
 
 test('test minutes in future with prefix', function (t) {
     t.plan(1);
-    t.equal(moment().add(5.5, 'minutes').short(), 'in 5m');
+    t.equal(moment().add(5, 'minutes').short(), 'in 5m');
 });
 
 test('test hours ago with suffix', function (t) {
     t.plan(1);
-    t.equal(moment().subtract(5.5, 'hours').short(), '5h ago');
+    t.equal(moment().subtract(5, 'hours').short(), '5h ago');
 });
 
 test('test hours in future with prefix', function (t) {
     t.plan(1);
-    t.equal(moment().add(5.5, 'hours').short(), 'in 5h');
+    t.equal(moment().add(5, 'hours').short(), 'in 5h');
 });
 
 test('test days ago with suffix', function (t) {
     t.plan(1);
-    t.equal(moment().subtract(5.5, 'days').short(), '5d ago');
+    t.equal(moment().subtract(5, 'days').short(), '5d ago');
 });
 
 test('test days in future with prefix', function (t) {
     t.plan(1);
-    t.equal(moment().add(5.5, 'days').short(), 'in 5d');
+    t.equal(moment().add(5, 'days').short(), 'in 5d');
 });
 
 test('test weeks ago', function (t) {
     t.plan(1);
-    var daysago = moment().subtract(8.5, 'days');
+    var daysago = moment().subtract(8, 'days');
     t.equal(daysago.short(), daysago.format('MMM D'));
 });
 
 test('test weeks in future', function (t) {
     t.plan(1);
-    var daysago = moment().add(8.5, 'days');
+    var daysago = moment().add(8, 'days');
     t.equal(daysago.short(), daysago.format('MMM D'));
 });
 
 test('test weeks last year', function (t) {
     t.plan(1);
-    var daysago = moment().subtract(1.1, 'year');
+    var daysago = moment().subtract(1, 'year');
     t.equal(daysago.short(), daysago.format('MMM D, YYYY'));
 });
 
 test('test weeks next year', function (t) {
     t.plan(1);
-    var daysago = moment().add(1.1, 'year');
+    var daysago = moment().add(1, 'year');
     t.equal(daysago.short(), daysago.format('MMM D, YYYY'));
 });
--- test/shortformat-without.js
+++ test/shortformat-without.js
@@ -3,64 +3,64 @@ var moment = require('../moment-shortformat');
 
 test('test seconds ago without suffix', function (t) {
     t.plan(1);
-    t.equal(moment().subtract(25.5, 'seconds').short(true), '25s');
+    t.equal(moment().subtract(25, 'seconds').short(true), '25s');
 });
 
 test('test seconds in future without prefix', function (t) {
     t.plan(1);
-    t.equal(moment().add(25.5, 'seconds').short(true), '25s');
+    t.equal(moment().add(25, 'seconds').short(true), '25s');
 });
 
 test('test minutes ago without suffix', function (t) {
     t.plan(1);
-    t.equal(moment().subtract(5.5, 'minutes').short(true), '5m');
+    t.equal(moment().subtract(5, 'minutes').short(true), '5m');
 });
 
 test('test minutes in future without prefix', function (t) {
     t.plan(1);
-    t.equal(moment().add(5.5, 'minutes').short(true), '5m');
+    t.equal(moment().add(5, 'minutes').short(true), '5m');
 });
 
 test('test hours ago without suffix', function (t) {
     t.plan(1);
-    t.equal(moment().subtract(5.5, 'hours').short(true), '5h');
+    t.equal(moment().subtract(5, 'hours').short(true), '5h');
 });
 
 test('test hours in future without prefix', function (t) {
     t.plan(1);
-    t.equal(moment().add(5.5, 'hours').short(true), '5h');
+    t.equal(moment().add(5, 'hours').short(true), '5h');
 });
 
 test('test days ago without suffix', function (t) {
     t.plan(1);
-    t.equal(moment().subtract(5.5, 'days').short(true), '5d');
+    t.equal(moment().subtract(5, 'days').short(true), '5d');
 });
 
 test('test days in future without prefix', function (t) {
     t.plan(1);
-    t.equal(moment().add(5.5, 'days').short(true), '5d');
+    t.equal(moment().add(5, 'days').short(true), '5d');
 });
 
 test('test weeks ago ignoring param', function (t) {
     t.plan(1);
-    var daysago = moment().subtract(8.5, 'days');
+    var daysago = moment().subtract(8, 'days');
     t.equal(daysago.short(true), daysago.format('MMM D'));
 });
 
 test('test weeks in future ignoring param', function (t) {
     t.plan(1);
-    var daysago = moment().add(8.5, 'days');
+    var daysago = moment().add(8, 'days');
     t.equal(daysago.short(true), daysago.format('MMM D'));
 });
 
 test('test weeks last year ignoring param', function (t) {
     t.plan(1);
-    var daysago = moment().subtract(1.1, 'year');
+    var daysago = moment().subtract(1, 'year');
     t.equal(daysago.short(true), daysago.format('MMM D, YYYY'));
 });
 
 test('test weeks next year ignoring param', function (t) {
     t.plan(1);
-    var daysago = moment().add(1.1, 'year');
+    var daysago = moment().add(1, 'year');
     t.equal(daysago.short(true), daysago.format('MMM D, YYYY'));
 });
