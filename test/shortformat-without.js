b92b877b533e519c03a6a99cb7fbb867ce575f65
Daniel Tschinder <daniel.tschinder@researchgate.net>
2016-11-01 11:46:55 +0100

    Migrate tests to ava and mock Date
    This makes the tests more stable.

--- test/shortformat-without.js
+++ test/shortformat-without.js
@@ -1,66 +1,72 @@
-var test = require('tape');
-var moment = require('../moment-shortformat');
+import test from 'ava';
+import moment from '../moment-shortformat';
 
-test('test seconds ago without suffix', function (t) {
-    t.plan(1);
-    t.equal(moment().subtract(25, 'seconds').short(true), '25s');
+test.beforeEach((t) => {
+    var RealDate = Date;
+    var customTimeMs = moment().valueOf();
+
+    function MockDate() {
+        return new RealDate(customTimeMs);
+    }
+    MockDate.now = () => new MockDate().valueOf();
+    MockDate.prototype = RealDate.prototype;
+
+    Date = MockDate;
+    t.context.RealDate = RealDate;
 });
 
-test('test seconds in future without prefix', function (t) {
-    t.plan(1);
-    t.equal(moment().add(25, 'seconds').short(true), '25s');
+test.afterEach((t) => {
+    Date = t.context.RealDate;
 });
 
-test('test minutes ago without suffix', function (t) {
-    t.plan(1);
-    t.equal(moment().subtract(5, 'minutes').short(true), '5m');
+test('test seconds ago without suffix', (t) => {
+    t.is(moment().subtract(25, 'seconds').short(true), '25s');
 });
 
-test('test minutes in future without prefix', function (t) {
-    t.plan(1);
-    t.equal(moment().add(5, 'minutes').short(true), '5m');
+test('test seconds in future without prefix', (t) => {
+    t.is(moment().add(25, 'seconds').short(true), '25s');
 });
 
-test('test hours ago without suffix', function (t) {
-    t.plan(1);
-    t.equal(moment().subtract(5, 'hours').short(true), '5h');
+test('test minutes ago without suffix', (t) => {
+    t.is(moment().subtract(5, 'minutes').short(true), '5m');
 });
 
-test('test hours in future without prefix', function (t) {
-    t.plan(1);
-    t.equal(moment().add(5, 'hours').short(true), '5h');
+test('test minutes in future without prefix', (t) => {
+    t.is(moment().add(5, 'minutes').short(true), '5m');
 });
 
-test('test days ago without suffix', function (t) {
-    t.plan(1);
-    t.equal(moment().subtract(5, 'days').short(true), '5d');
+test('test hours ago without suffix', (t) => {
+    t.is(moment().subtract(5, 'hours').short(true), '5h');
 });
 
-test('test days in future without prefix', function (t) {
-    t.plan(1);
-    t.equal(moment().add(5, 'days').short(true), '5d');
+test('test hours in future without prefix', (t) => {
+    t.is(moment().add(5, 'hours').short(true), '5h');
 });
 
-test('test weeks ago ignoring param', function (t) {
-    t.plan(1);
+test('test days ago without suffix', (t) => {
+    t.is(moment().subtract(5, 'days').short(true), '5d');
+});
+
+test('test days in future without prefix', (t) => {
+    t.is(moment().add(5, 'days').short(true), '5d');
+});
+
+test('test weeks ago ignoring param', (t) => {
     var daysago = moment().subtract(8, 'days');
-    t.equal(daysago.short(true), daysago.format('MMM D'));
+    t.is(daysago.short(true), daysago.format('MMM D'));
 });
 
-test('test weeks in future ignoring param', function (t) {
-    t.plan(1);
+test('test weeks in future ignoring param', (t) => {
     var daysago = moment().add(8, 'days');
-    t.equal(daysago.short(true), daysago.format('MMM D'));
+    t.is(daysago.short(true), daysago.format('MMM D'));
 });
 
-test('test weeks last year ignoring param', function (t) {
-    t.plan(1);
-    var daysago = moment().subtract(1, 'year');
-    t.equal(daysago.short(true), daysago.format('MMM D, YYYY'));
+test('test weeks last year ignoring param', (t) => {
+    var daysago = moment().subtract(370, 'days');
+    t.is(daysago.short(true), daysago.format('MMM D, YYYY'));
 });
 
-test('test weeks next year ignoring param', function (t) {
-    t.plan(1);
-    var daysago = moment().add(1, 'year');
-    t.equal(daysago.short(true), daysago.format('MMM D, YYYY'));
+test('test weeks next year ignoring param', (t) => {
+    var daysago = moment().add(370, 'days');
+    t.is(daysago.short(true), daysago.format('MMM D, YYYY'));
 });
